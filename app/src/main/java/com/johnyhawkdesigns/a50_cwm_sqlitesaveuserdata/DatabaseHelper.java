f4db48c103a15f33b1466928ddb486e4b5af3c35
Ahsan <johnyhawkahsan@gmail.com>
2018-07-23 15:10:04 +0500

    2nd Commit- Project Completed. Although it was a simple project, the previous project was more extensive and in detail.

--- app/src/main/java/com/johnyhawkdesigns/a50_cwm_sqlitesaveuserdata/DatabaseHelper.java
+++ app/src/main/java/com/johnyhawkdesigns/a50_cwm_sqlitesaveuserdata/DatabaseHelper.java
@@ -2,14 +2,18 @@ package com.johnyhawkdesigns.a50_cwm_sqlitesaveuserdata;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * Created by Ahsan on 7/20/2018.
  */
 
 public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String TAG = "DatabaseHelper";
 
     public static final String DATABASE_NAME = "people.db";
     public static final String TABLE_NAME = "people_table";
@@ -27,6 +31,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         String createTable = "CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " + "NAME TEXT, EMAIL TEXT, TVSHOW TEXT)";
         db.execSQL(createTable);
+        Log.d(TAG, "createTable: " + createTable);
 
     }
 
@@ -43,9 +48,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public Boolean addData(String name, String email, String tvShow){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(COL1, name);
-        contentValues.put(COL2, email);
-        contentValues.put(COL3, tvShow);
+        contentValues.put(COL2, name);
+        contentValues.put(COL3, email);
+        contentValues.put(COL4, tvShow);
         long result = db.insert(TABLE_NAME, null, contentValues);
 
         if (result == -1){
@@ -55,6 +60,44 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         }
     }
 
+
+    /**
+     * @retrun Cursor
+     * View Data by searching all Table
+     */
+    public Cursor viewData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "Select * from " + TABLE_NAME;
+        Cursor data = db.rawQuery(query,null);
+        return data;
+
+    }
+
+    /**
+     * @return Boolean
+     * Update data based on id argument
+     */
+    public boolean updateData(String id, String name, String email, String tvShow){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL1, id);
+        contentValues.put(COL2, name);
+        contentValues.put(COL3, email);
+        contentValues.put(COL4, tvShow);
+        db.update(TABLE_NAME, contentValues, "ID = ?", new String[]{id}); //Where Clause is "ID = ?" while where args is an array form id
+        return true;
+
+    }
+
+
+    /**
+     * @return Integer
+     * Delete data from database based on id
+     */
+    public Integer deleteData(String id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_NAME, "ID = ?", new String[]{id});
+    }
 
 
 }
--- app/src/main/java/com/johnyhawkdesigns/a50_cwm_sqlitesaveuserdata/MainActivity.java
+++ app/src/main/java/com/johnyhawkdesigns/a50_cwm_sqlitesaveuserdata/MainActivity.java
@@ -1,5 +1,7 @@
 package com.johnyhawkdesigns.a50_cwm_sqlitesaveuserdata;
 
+import android.app.AlertDialog;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -31,11 +33,18 @@ public class MainActivity extends AppCompatActivity {
         etID = (EditText) findViewById(R.id.etID);
         btnDelete = (Button) findViewById(R.id.btnDelete);
 
-        addData();
 
+        //This is something new for me. All those methods, there is onClick method
+        addData();
+        viewData();
+        updateData();
+        deleteData();
     }
 
 
+
+
+    //Method for Add Data button to add Data to our Database
     public void addData(){
         btnAddData.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,4 +64,110 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
+
+
+
+    //Method to View data
+    public void viewData() {
+        btnViewData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Cursor data = peopleDB.viewData();
+
+                if (data.getCount() == 0){
+                    displayMessage("Error", "No Data Found, data.getCount() == 0");
+                }
+
+                StringBuffer stringBuffer = new StringBuffer();
+                //Iterate through our data
+                while (data.moveToNext()){
+                    stringBuffer.append("ID: " + data.getString(0) + "\n");
+                    stringBuffer.append("Name: " + data.getString(1) + "\n");
+                    stringBuffer.append("Email: " + data.getString(2) + "\n");
+                    stringBuffer.append("Favourite TV Show: " + data.getString(3) + "\n");
+
+                    //Display message
+                    displayMessage("All Stored Data: ", stringBuffer.toString());
+                }
+
+            }
+        });
+
+    }
+
+
+
+    //Method to update data
+    public void updateData(){
+        btnUpdateData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int temp = etID.getText().toString().length();
+                if (temp > 0){
+
+                    String id = etID.getText().toString();
+                    String name = etName.getText().toString();
+                    String email = etEmail.getText().toString();
+                    String tvShow = etTVShow.getText().toString();
+
+                    boolean update = peopleDB.updateData(id, name, email, tvShow);
+                    if (update){
+                        Toast.makeText(MainActivity.this, "Data updated successfully", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Something went wrong while updating", Toast.LENGTH_LONG).show();
+                    }
+
+                } else {
+                    Toast.makeText(MainActivity.this, "You must enter an ID to Update", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+    }
+
+
+    private void deleteData() {
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int temp = etID.getText().toString().length();
+                String id = etID.getText().toString();
+                if (temp > 0){
+                    Integer deletedRow = peopleDB.deleteData(id);
+                    if (deletedRow > 0){
+                        Toast.makeText(MainActivity.this, deletedRow + " rows deleted successfully", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, deletedRow + " ,something went wrong", Toast.LENGTH_LONG).show();
+                    }
+
+                } else {
+                    Toast.makeText(MainActivity.this, "You must enter an ID to Delete", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+    }
+
+
+    //Alert Dialog Builder Method
+    public void displayMessage(String title, String message){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setCancelable(true);
+        alertDialogBuilder.setTitle(title);
+        alertDialogBuilder.setMessage(message);
+        alertDialogBuilder.show();
+
+    }
+
+
+
+
+
+
+
+
+
+
 }
